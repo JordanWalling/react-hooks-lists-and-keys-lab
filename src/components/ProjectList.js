9aee5c78bd58a49c1c7732c5ba25c9f2144a67e9
JordanWalling <jordanwalling1991@gmail.com>
2024-02-19 05:52:30 +1100

    complete lists and keys lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,20 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((project) => {
+          const { name, id, technologies } = project;
+          return (
+            <ProjectItem
+              key={id}
+              id={id}
+              name={name}
+              technologies={technologies}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
